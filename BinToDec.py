416c7b96f0248987bbf7acfabba7af7306e5b45d
Rhythm Juneja <66195447+Rhythm7845@users.noreply.github.com>
2023-04-03 21:42:27 +0530

    Update BinToDec.py

--- BinToDec.py
+++ BinToDec.py
@@ -1,27 +1,21 @@
-def reverse(x):
-    return(x[::-1])
+#Function to reverse strings. This is used to reverse the binary number so that it can be read from left to right.
+def reverse(temp_binary):
+    return(temp_binary[::-1])
 
-def bintodec():
-    bin = str(reverse(input("Enter a number in binary: ")));n = len(bin);a = 0;dec = 0
+#Inputs a binary number and reverses it using the above function. Also initializes the variables used in the loop.
 
-    while (a < n):
-        b = bool(int(bin[a]))
-        if(b == True):
-            dec = dec + 2**a
-        a = a + 1
-    print("The number in decimal form is:", dec)
+binary_num = str(reverse(input("Enter power number in binary: ")));n = len(binary_num)
+power = 0
+decimal_number = 0
+
+#Converts the binary number to decimal_number. Can be done via recursion, but I chose to do it this way.
+
+while (power < n):
+    bin_bool = bool(int(binary_num[power]))    #bin_bool is a boolean variable that is True if the digit of the inary number is 1 and False if the digit is 0.
+    if(bin_bool == True):
+        decimal_number = decimal_number + 2**power   #Adds 2 to the power of the digit to the decimal number.
+    power +=1
     
-def dectobin(n):
-    return "{0:b}".format(int(n))
+#Prints the decimal_number number.
 
-choice = input("Do you want to enter a decimal number or binary number? d/b: ")
-if(choice == "d"):
-    n = int(input("Enter a decimal number: "))
-    print("The decimal form is:", dectobin(n))
-    quit()
-elif(choice == "b"):
-    bintodec()
-    quit()
-else:
-    print("Enter d or b only")
-    quit()
+print("The number in decimal_number form is:", decimal_number)
